97193a5deb17848815579c5405767fbf2492195c
Bryan Keener <bkeener7@gmail.com>
2022-09-02 14:01:57 -0600

    Complete iteration 2

---
+++ lib/dealership.rb
@@ -0,0 +1,18 @@
+class Dealership
+    attr_reader :inventory
+
+    def initialize(name, address)
+        @name = name
+        @address = address
+        @inventory = []
+    end
+
+    def inventory_count
+        inventory.length
+    end
+
+    def add_car(car)
+        @inventory.push(car)
+    end
+
+end
